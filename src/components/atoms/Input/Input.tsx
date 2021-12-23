4fe5a13fe72874a42f7f6f5a7134ad19043ea1c0
dblee <danbi.db@gmail.com>
2021-12-23 20:12:44 +0900

    feat: Add input atom

---
+++ src/components/atoms/Input/Input.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import styles from './Input.module.scss';
+import { InputType } from '../atom';
+
+interface InputProps {
+  type: InputType;
+  placeholder?: string;
+}
+
+const Input: React.FC<InputProps> = ({ ...props }) => {
+  return <input className={styles.input} {...props} />;
+};
+
+export default Input;
--- src/components/atoms/atom.d.ts
+++ src/components/atoms/atom.d.ts
@@ -4,3 +4,6 @@ export type FontWeight = 'light' | 'normal' | 'medium' | 'bold' | 'black';
 
 // Color
 export type Color = 'white' | 'gray-200' | 'gray-300' | 'gray-400' | 'gray-500' | 'black' | 'blue' | 'blue-inactive';
+
+// Input
+export type InputType = 'text' | 'password';
